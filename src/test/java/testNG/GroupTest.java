53427e13415e192262dadb5352e3c7421dd40e74
Sassri24 <chamodganegoda@gmail.com>
2024-10-08 18:58:23 +0530

    make xml file for running gruptest

--- src/test/java/testNG/GroupTest.java
+++ src/test/java/testNG/GroupTest.java
@@ -6,11 +6,11 @@ public class GroupTest {
     //S= smoke testing
     //R= regression testing
 
-    @Test
+    @Test(groups = {"S","R"})
     public void test01(){
         System.out.println("This is Smoke and Regression test case");
     }
-    @Test
+    @Test(groups = {"R"})
     public void test02(){
         System.out.println("This is Regression test case");
     }
@@ -18,11 +18,11 @@ public class GroupTest {
     public void test03(){
         System.out.println("This is Not Smoke or Regression");
     }
-    @Test
+    @Test(groups = {"S"})
     public void test04(){
         System.out.println("This is Smoke test case");
     }
-    @Test
+    @Test(groups = {"S","R"})
     public void test05(){
         System.out.println("This is Smoke and Regression test case");
     }
