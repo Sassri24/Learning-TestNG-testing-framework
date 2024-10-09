6348215f38a81321dcb3fd1ff58f060b48c1950e
Sassri24 <chamodganegoda@gmail.com>
2024-10-09 21:20:03 +0530

    DataProvider

--- src/test/java/testNG/DataProvider/DataProviderTest.java
+++ src/test/java/testNG/DataProvider/DataProviderTest.java
@@ -1,7 +1,14 @@
 package testNG.DataProvider;
 
+import org.testng.annotations.DataProvider;
+
 public class DataProviderTest {
 
+    public void loginTest(String name,String pass){
+        System.out.println(name+" "+pass);
+    }
+
+    @DataProvider
     public Object[][] getData(){
         Object[][] data={
                 {"chamod@gmail.com","chaod123"},
