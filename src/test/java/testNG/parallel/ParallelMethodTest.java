35335933b4cdef63ccac7d0f08719676ec68f7e1
Sassri24 <chamodganegoda@gmail.com>
2024-10-09 22:47:31 +0530

    Parallel testing done

---
+++ src/test/java/testNG/parallel/ParallelMethodTest.java
@@ -0,0 +1,22 @@
+package testNG.parallel;
+
+import org.testng.annotations.Test;
+
+public class ParallelMethodTest {
+
+    @Test(priority = 0)
+    public void openGoogle(){
+        System.out.println("open google");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.quit();
+    }
+
+    @Test(priority = 1)
+    public void openBing(){
+        System.out.println("open google");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.bing.com/");
+        driver.quit();
+    }
+}
