33b9cdac1fa6f829a6951fba4c847d126dd026c4
Sassri24 <chamodganegoda@gmail.com>
2024-10-09 21:00:28 +0530

    start parameters

--- src/test/java/testNG/parameter/ParameterTest03.java
+++ src/test/java/testNG/parameter/ParameterTest03.java
@@ -1,4 +1,20 @@
 package testNG.parameter;
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class ParameterTest03 {
+
+    @Test
+    @Parameters("browser")
+    public void browserExecution(String browser){
+        System.out.println("TestNG XML file passed browser value  is :"+browser);
+        if(browser.equalsIgnoreCase("Chrome")){
+            System.out.println("Browser should open with chrome");
+        } else if (browser.equalsIgnoreCase("edge")){
+            System.out.println("Browser should open with edge");
+        } else {
+            System.out.println("Browser should open with Firefox");
+        }
+    }
 }
