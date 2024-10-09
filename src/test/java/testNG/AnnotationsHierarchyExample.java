6dcec5a91446490886b348753237d99d0a7e1a17
Sassri24 <chamodganegoda@gmail.com>
2024-10-09 22:22:09 +0530

    Annotations hierarchy ends

--- src/test/java/testNG/AnnotationsHierarchyExample.java
+++ src/test/java/testNG/AnnotationsHierarchyExample.java
@@ -5,31 +5,42 @@ import org.testng.annotations.*;
 public class AnnotationsHierarchyExample {
     @Test
     public void test01() {
+        System.out.println("I am Test01");
+    }
+
+    @Test
+    public void test02() {
+        System.out.println("I am Test02");
     }
 
     @BeforeMethod
     public void beforeMethod() {
+        System.out.println("I am before Method");
+    }
 
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("I am after Method");
     }
 
     @BeforeClass
     public void beforeClass() {
-
+        System.out.println("I am before class");
     }
 
     @AfterClass
     public void afterClass() {
-
+        System.out.println("I am after class");
     }
 
     @BeforeTest
     public void beforeTest(){
-        System.out.println("I am before Test");
+        System.out.println("I am before test");
     }
 
     @AfterTest
     public void afterTest(){
-        System.out.println("I am After Test");
+        System.out.println("I am after Test");
     }
 
     @BeforeSuite
